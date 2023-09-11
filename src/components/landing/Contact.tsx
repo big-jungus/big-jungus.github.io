3c9c214c097757e2368c47cb2c2484c98176ba0e
big-jungus <matthewjung1010@gmail.com>
2023-09-11 16:54:33 -0700

    project buttons

--- src/components/landing/Contact.tsx
+++ src/components/landing/Contact.tsx
@@ -11,14 +11,14 @@ function Contact(){
             <h1 className="sectionHeading">CONTACT</h1>
 
             <div className="contactImagesContainer">
-                <a>
-                    <img className="links" src={email} width={110} height={110}/>
+                <a href="mailto: matthewjung1010@gmail.com" target="_blank">
+                    <img className="contactImage links" src={email} width={110} height={110}/>
                 </a>
-                <a>
-                    <img className="" src={linkedin}/>
+                <a href="https://www.linkedin.com/in/matthewjung1010/" target="_blank">
+                    <img className="contactImage " src={linkedin}/>
                 </a>
-                <a>
-                    <img className="" src={resume}/>
+                <a href="" target="_blank">
+                    <img className="contactImage " src={resume}/>
                 </a>
             </div>
         </div>
