628a76cffac4d8a85795d89f63e28a6947e6e633
big-jungus <matthewjung1010@gmail.com>
2023-09-11 21:54:51 -0700

    small about changes

--- src/components/landing/About.tsx
+++ src/components/landing/About.tsx
@@ -15,8 +15,8 @@ function About() {
                 </div>
                 <div>
                     <p className="aboutParagraph">
-                        I'm currently a senior at UC Irvine, majoring in Game Design and Interactive Media, with experience in Unity, C#, and Python. I find gameplay design and combat design to be really interesting, and have been designing games for a variety of genres. <br/><br/>
-                        I've been playing games for as long as I remember, and games have had a huge impact on defining who I am today. Some of my favorite games of all time are Super Mario Galaxy, Chrono Trigger, Slay the Spire, Overwatch, Risk of Rain 2, Noita, and Elden Ring!<br/><br/>
+                        I'm currently a senior at UC Irvine, majoring in Game Design and Interactive Media, with experience in Unity, Unreal, C#, C++, and Python. I find gameplay design and combat design to be really interesting, and have been designing games for a variety of genres. <br/><br/>
+                        I've been playing games for as long as I remember, and games have had a huge impact on defining who I am today. Some of my favorite games of all time are Super Mario Galaxy, Chrono Trigger, Slay the Spire, League of Legends, Risk of Rain 2, Noita, and CS:GO!<br/><br/>
                         Whether through thrilling gameplay, brain racking mechanics, or compelling stories, I knew I wanted to recreate the experiences I had for the next generations of gamers.<br/><br/>
                         During the last few years at UC Irvine, I've gone from creating simple games on my own, to working with small scale teams to produce polished games for others to enjoy!
                     </p>    
