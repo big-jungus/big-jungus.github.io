8beb68ced51de4def4fadb53bc83a1e72110cb45
big-jungus <matthewjung1010@gmail.com>
2023-09-12 11:54:43 -0700

    about section updates and resume fix

--- src/components/landing/About.tsx
+++ src/components/landing/About.tsx
@@ -15,10 +15,10 @@ function About() {
                 </div>
                 <div>
                     <p className="aboutParagraph">
-                        I'm currently a senior at UC Irvine, majoring in Game Design and Interactive Media, with experience in Unity, Unreal, C#, C++, and Python. I find gameplay design and combat design to be really interesting, and have been designing games for a variety of genres. <br/><br/>
-                        I've been playing games for as long as I remember, and games have had a huge impact on defining who I am today. Some of my favorite games of all time are Super Mario Galaxy, Chrono Trigger, Slay the Spire, League of Legends, Risk of Rain 2, Noita, and CS:GO!<br/><br/>
-                        Whether through thrilling gameplay, brain racking mechanics, or compelling stories, I knew I wanted to recreate the experiences I had for the next generations of gamers.<br/><br/>
-                        During the last few years at UC Irvine, I've gone from creating simple games on my own, to working with small scale teams to produce polished games for others to enjoy!
+                        I'm currently a senior at UC Irvine, majoring in Game Design and Interactive Media, with experience in Unity, Unreal, C#, C++, and Python. I find combat systems in games to be especially interesting, and have been creating games for a variety of genres. <br/><br/>
+                        I've been playing games for as long as I remember, and games have had a huge impact on defining who I am today. Some of my favorite games of all time (in no particular order) are Super Mario Galaxy, Chrono Trigger, Slay the Spire, League of Legends, Risk of Rain 2, Noita, and CS:GO!<br/><br/>
+                        These games hold a special place in my heart, and I still remember the chills of playing through each of these games for the first time. Whether through thrilling gameplay, brain racking mechanics, or compelling stories, I knew I wanted to recreate the same feelings I had for the next generations of gamers.<br/><br/>
+                        I aim to push the boundaries on gaming, creating new gameplay experiences that inspire gamers and developers alike!
                     </p>    
                 </div>
             </div>
