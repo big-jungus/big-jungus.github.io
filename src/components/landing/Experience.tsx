6f87127ca0f989f22812b51cb2526391897e9f7c
big-jungus <matthewjung1010@gmail.com>
2024-03-11 14:20:09 -0700

    layout fixes

--- src/components/landing/Experience.tsx
+++ src/components/landing/Experience.tsx
@@ -1,33 +1,43 @@
 import "./css/stylesHome.css"
 
+import { Button } from "react-bootstrap";
+
 function Experience () {
     return (
         <div className="experienceBackground">
             <div className="anchor" id="experience"/>
             <h1 className="sectionHeading experienceHeading">WORK EXPERIENCE</h1>
 
-            <div className="projectSection">
-                <div className="projectHeaderLeft">
-                    <a href="https://www.saic.com" target="_blank">
-                        <h2>SAIC</h2>
-                    </a>
+            <div className="projectHolder">
+                <div className="projectSection">
+                    <div className="projectHeaderLeft">
+                        <a href="https://www.saic.com" target="_blank">
+                            <h2>SAIC</h2>
+                        </a>
+                        
+                        <div>
+                            <h3>Game Programming Intern</h3>
+                            <h3>June 2023 - August 2023</h3>
+                            <h3>Team: <span className="highlight">5</span></h3>
+                        </div>
+                    </div>
                     
-                    <div>
-                        <h3>Game Programming Intern</h3>
-                        <h3>June 2023 - August 2023</h3>
-                        <h3>Team: <span className="highlight">5</span></h3>
+                    <div className="projectDescription">
+                        <Button className="buttonLeft">VIEW WEBSITE</Button>
+                        <div className="projectBodyLeft">
+                            <ul>
+                                <p>This was a confidential project, making a realistic turn-based wargame based on a near-future scenario.</p>
+                                <li>Tools: <span className="highlight">Unreal</span>, Git, Blueprints, C++</li>
+                                <li>Focused on <span className="highlight">developing the multiplayer framework</span> and implementing all UI into Unreal including <span className="highlight">menus, character selection, and HUD</span> .</li>
+                                <li>Worked closely with two engineers to incorporate live Matlab generated data into the game.</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
-                
-                <div className="projectBodyLeft">
-                    <ul>
-                        <p>This was a confidential project, making a realistic turn-based wargame based on a near-future scenario.</p>
-                        <li>Tools: <span className="highlight">Unreal</span>, Git, Blueprints, C++</li>
-                        <li>Focused on <span className="highlight">developing the multiplayer framework</span> and implementing all UI into Unreal including <span className="highlight">menus, character selection, and HUD</span> .</li>
-                        <li>Worked closely with two engineers to incorporate live Matlab generated data into the game.</li>
-                    </ul>
-                </div>
             </div>
+
+
+
             <div className="padding"/>
         </div>
     );
--- src/components/landing/ProjectsRecent.tsx
+++ src/components/landing/ProjectsRecent.tsx
@@ -22,76 +22,94 @@ const Projects = ({
                 SELECTED GAMES
             </div>
 
-            <div className="projectSection">
-                <div className="projectHeaderLeft">
-                    <div className="projectTitle">
+            <div className="projectHolder">
+                <div className="projectSection">
+                    <div className="projectHeaderLeft">
                         <h2>PROJECT: ETHER</h2>
-                        <Button>Play Now!</Button>
+                        
+                        <div>
+                            <h3>UI/Gameplay Programmer</h3>
+                            <h3>In Progress</h3>
+                            <h3>Team: <span className="highlight">40</span></h3>
+                        </div>
                     </div>
                     
-                    <div>
-                        <h3>UI/Gameplay Programmer</h3>
-                        <h3>In Progress</h3>
-                        <h3>Team: <span className="highlight">40</span></h3>
+                    <div className="projectDescription">
+                        <Button bsPrefix="buttonLeft">PLAY NOW!</Button>
+                        <div className="projectBodyLeft">
+                            <ul>
+                                <p>Action-Stealth game focused on using magical abilities to outwit your enemies!</p>
+                                <li>Tools: <span className="highlight">Unreal</span>, Figma, Perforce, Blueprints, C++, Airtable</li>
+                                <li><span className="highlight">Worked alongside with UX designers and UI artists</span> to implement each iteration of their designs into engine, including <span className="highlight"> all menus, HUD and more!</span></li>
+                                <li>Created multiple iterations of various accessibility features, such as <span className="highlight">subtitles, colorblind modes, aim assist, and high contrast display</span>.</li>
+                            </ul>
+                        </div>
                     </div>
-
-                    <img src={ETH_Icon} className="projectIcon"></img>
                 </div>
-                
-                <div className="projectBodyLeft">
-                    <ul>
-                        <p>Action-Stealth game focused on using magical abilities to outwit your enemies!</p>
-                        <li>Tools: <span className="highlight">Unreal</span>, Figma, Perforce, Blueprints, C++, Airtable</li>
-                        <li><span className="highlight">Worked alongside with UX designers and UI artists</span> to implement each iteration of their designs into engine, including <span className="highlight"> all menus, HUD and more!</span></li>
-                        <li>Created multiple iterations of various accessibility features, such as <span className="highlight">subtitles, colorblind modes, aim assist, and high contrast display</span>.</li>
-                    </ul>
+
+                <div className="projectImages">
+                        <img src={ETH_Icon} className="projectIcon"></img>
                 </div>
             </div>
 
-            <div className="projectSection">
-                <div className="projectHeaderRight">
-                    <h2>EDO EDO TEISHOKU</h2>
-                    
-                    <div>
-                        <h3>Gameplay Programmer</h3>
-                        <h3>In Progress</h3>
-                        <h3>Team: <span className="highlight">43</span></h3>
+            <div className="projectHolder">
+                <div className="projectSection">
+                    <div className="projectHeaderRight">
+                        <h2>EDO EDO TEISHOKU</h2>
+                        
+                        <div>
+                            <h3>Gameplay Programmer</h3>
+                            <h3>In Progress</h3>
+                            <h3>Team: <span className="highlight">43</span></h3>
+                        </div>
                     </div>
-
-                    <img src={EDO_Icon} className="projectIcon"></img>
+                    
+                    <div className="projectDescription">
+                        <Button className="buttonRight">PLAY NOW!</Button>
+                        <div className="projectBodyRight">
+                            <ul>
+                                <p>Rogue-like card game focused on cooking up dishes to defeat animated foods.</p>
+                                <li>Tools: <span className="highlight">Unity</span>, Figma, Git, C#, Airtable</li>
+                                <li>Implemented all of the game's bosses, working with game designers to <span className="highlight">update bosses during each iteration of the game's system design</span>.</li>
+                                <li>Worked with a UX designer to <span className="highlight">create the game's tutorial to walk players through the entire process of the game's core loop</span>.</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-                
-                <div className="projectBodyRight">
-                    <ul>
-                        <p>Rogue-like card game focused on cooking up dishes to defeat animated foods.</p>
-                        <li>Tools: <span className="highlight">Unity</span>, Figma, Git, C#, Airtable</li>
-                        <li>Implemented all of the game's bosses, working with game designers to <span className="highlight">update bosses during each iteration of the game's system design</span>.</li>
-                        <li>Worked with a UX designer to <span className="highlight">create the game's tutorial to walk players through the entire process of the game's core loop</span>.</li>
-                    </ul>
+
+                <div className="projectImages">
+                        <img src={EDO_Icon} className="projectIcon"></img>
                 </div>
             </div>
 
-            <div className="projectSection">
-                <div className="projectHeaderLeft">
-                    <h2>LIGHT-RUNNER</h2>
-                    
-                    <div>
-                        <h3>UI/Gameplay Programmer</h3>
-                        <h3>April 2023 - June 2023</h3>
-                        <h3>Team: <span className="highlight">4</span></h3>
+            <div className="projectHolder">
+                <div className="projectSection">
+                    <div className="projectHeaderLeft">
+                        <h2>LIGHT-RUNNER</h2>
+                        
+                        <div>
+                            <h3>UI/Gameplay Programmer</h3>
+                            <h3>April 2023 - June 2023</h3>
+                            <h3>Team: <span className="highlight">4</span></h3>
+                        </div>
                     </div>
-
-                    <img src={LR_Icon} className="projectIcon"></img>
+                    
+                    <div className="projectDescription">
+                        <Button className="buttonLeft">PLAY NOW!</Button>
+                        <div className="projectBodyLeft">
+                            <ul>
+                                <p>1 vs. 1 arena shooter focused on movement mechanics.</p>
+                                <li>Tools: <span className="highlight">Unreal</span>, Perforce, Blueprints, C++</li>
+                                <li>Designed and implemented the game's movement systems such as wall-running and dynamic sliding.</li>
+                                <li><span className="highlight">Gathered feedback from multiple playtests and iterated on designs</span> to make the movement feel both smooth and thrilling for players.</li>
+                                <li>This was my first game made in Unreal Engine 5, and was a huge <span className="highlight">learning experience for both Unreal and multiplayer programming</span>.</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-                
-                <div className="projectBodyLeft">
-                    <ul>
-                        <p>1 vs. 1 arena shooter focused on movement mechanics.</p>
-                        <li>Tools: <span className="highlight">Unreal</span>, Perforce, Blueprints, C++</li>
-                        <li>Designed and implemented the game's movement systems such as wall-running and dynamic sliding.</li>
-                        <li><span className="highlight">Gathered feedback from multiple playtests and iterated on designs</span> to make the movement feel both smooth and thrilling for players.</li>
-                        <li>This was my first game made in Unreal Engine 5, and was a huge <span className="highlight">learning experience for both Unreal and multiplayer programming</span>.</li>
-                    </ul>
+
+                <div className="projectImages">
+                        <img src={LR_Icon} className="projectIcon"></img>
                 </div>
             </div>
 
