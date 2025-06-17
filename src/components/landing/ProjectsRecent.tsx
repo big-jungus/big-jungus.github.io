ffc476393a44b2341c1adaa99a367ad231e25935
big-jungus <matthewjung1010@gmail.com>
2025-06-17 15:20:29 -0700

    added pitaya, removed edo

--- src/components/landing/ProjectsRecent.tsx
+++ src/components/landing/ProjectsRecent.tsx
@@ -2,11 +2,11 @@ import "./css/stylesHome.css"
 import ETH_Icon from "/projectPictures/Ether/ETH6.png";
 import ETH_Icon2 from "/projectPictures/Ether/ETH7.png";
 
-import EDO_Icon from "/projectPictures/EdoEdo/EDO7.png";
-import EDO_Icon2 from "/projectPictures/EdoEdo/EDO5.png";
-
 import LR_Icon from "/projectPictures/LightRunner/LR1.jpg";
 import LR_Icon2 from "/projectPictures/LightRunner/LR2.gif";
+
+import Pitaya_Icon1 from "/projectPictures/Pitaya/Pitaya_HUD.png"
+import Pitaya_Icon2 from "/projectPictures/Pitaya/Pitaya_Skill_Tree.png"
 
 import { Button } from "react-bootstrap";
 
@@ -30,6 +30,38 @@ const Projects = ({
             <div className="projectHolder">
                 <div className="projectSection">
                     <div className="projectHeaderLeft">
+                        <h2>PITAYA!</h2>
+                        
+                        <div>
+                            <h3>UI Programmer</h3>
+                            <h3>October 2024 - Present</h3>
+                            <h3>Team: <span className="highlight">8</span></h3>
+                        </div>
+                    </div>
+                    
+                    <div className="projectDescription">
+                        <Button bsPrefix="buttonLeft" onClick={()=> window.open("https://store.steampowered.com/app/3754290/Pitaya/", "_blank")}>PLAY NOW!</Button>
+                        <div className="projectBodyLeft">
+                            <ul>
+                                <p>A 3rd-Person Action RPG about a cursed girl who needs to eat to survive!</p>
+                                <li>Tools: <span className="highlight">Unreal</span>, Figma, Perforce, <span className="highlight">Blueprints</span>, <span className="highlight">C++</span>, UMG</li>
+                                <li>Worked closely with team’s designers and artists to design UI in Figma that fit the game’s playful aesthetic.</li>
+                                <li>Created and implemented all UI elements and animations, and added compatability to both <span className="highlight">gamepad</span> and <span className="highlight">keyboard and mouse</span>.</li>
+                                <li>Create other features, such as <span className="highlight">gamepad brand detection</span>.</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="projectImages">
+                        <img src={Pitaya_Icon1} className="projectIcon"></img>
+                        <img src={Pitaya_Icon2} className="projectIcon"/>
+                </div>
+            </div>
+
+            <div className="projectHolder">
+                <div className="projectSection">
+                    <div className="projectHeaderLeft">
                         <h2>PROJECT: ETHER</h2>
                         
                         <div>
@@ -40,11 +72,11 @@ const Projects = ({
                     </div>
                     
                     <div className="projectDescription">
-                        <Button bsPrefix="buttonLeft" onClick={()=> window.open("https://pjheric.itch.io/project-ether", "_blank")}>PLAY NOW!</Button>
+                        <Button bsPrefix="buttonLeft" onClick={()=> window.open("https://store.steampowered.com/app/2956670/Project_Ether/?beta=0", "_blank")}>PLAY NOW!</Button>
                         <div className="projectBodyLeft">
                             <ul>
                                 <p>Action-Stealth game focused on using magical abilities to outwit your enemies!</p>
-                                <li>Tools: <span className="highlight">Unreal</span>, Figma, Perforce, Blueprints, C++, Airtable</li>
+                                <li>Tools: <span className="highlight">Unreal</span>, Figma, Perforce, <span className="highlight">Blueprints</span>, <span className="highlight">C++</span>, Airtable</li>
                                 <li><span className="highlight">Worked alongside with UX designers and UI artists</span> to implement each iteration of their designs into engine, including <span className="highlight"> all menus, HUD and more!</span></li>
                                 <li>Created multiple iterations of various accessibility features, such as <span className="highlight">subtitles, colorblind modes, aim assist, and high contrast display</span>.</li>
                                 <li>Developed both the <span className="highlight">Save & Load system</span> and the <span className="highlight">Objective system</span> for the game as well!</li>
@@ -56,37 +88,6 @@ const Projects = ({
                 <div className="projectImages">
                         <img src={ETH_Icon} className="projectIcon"></img>
                         <img src={ETH_Icon2} className="projectIcon"/>
-                </div>
-            </div>
-
-            <div className="projectHolder">
-                <div className="projectSection">
-                    <div className="projectHeaderRight">
-                        <h2>EDO EDO TEISHOKU</h2>
-                        
-                        <div>
-                            <h3>Gameplay Programmer</h3>
-                            <h3>August 2023 - March 2023</h3>
-                            <h3>Team: <span className="highlight">43</span></h3>
-                        </div>
-                    </div>
-                    
-                    <div className="projectDescription">
-                        <Button bsPrefix="buttonRight" onClick={()=> window.open("https://funbam.itch.io/edo-edo-teishoku", "_blank")}>PLAY NOW!</Button>
-                        <div className="projectBodyRight">
-                            <ul>
-                                <p>Rogue-like card game focused on cooking up dishes to defeat animated foods.</p>
-                                <li>Tools: <span className="highlight">Unity</span>, Figma, Git, C#, Airtable</li>
-                                <li>Implemented all 5 of the game's bosses, working with game designers to <span className="highlight">update bosses during each iteration of the game's system design</span>.</li>
-                                <li>Worked with a UX designer to <span className="highlight">create the game's tutorial to walk players through the entire process of the game's core loop</span>.</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="projectImages">
-                        <img src={EDO_Icon} className="projectIcon"></img>
-                        <img src={EDO_Icon2} className="projectIcon"></img>
                 </div>
             </div>
 
